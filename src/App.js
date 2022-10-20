c88dd88875999ee58ad9ceea281ee313dfda577a
alejandron103 <alejandro.naranjo@cun.edu.co>
2022-10-20 16:26:21 -0500

    fix: ios behavior

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [isReadyForInstall, setIsReadyForInstall] = useState(false);
+  const [isIOS, setIsIOS] = useState(false)
+
+  useEffect(() => {
+    getOperatingSystem()
+    window.addEventListener("beforeinstallprompt", (event) => {
+      // Prevent the mini-infobar from appearing on mobile.
+      event.preventDefault();
+      console.log("👍", "beforeinstallprompt", event);
+      // Stash the event so it can be triggered later.
+      window.deferredPrompt = event;
+      // Remove the 'hidden' class from the install button container.
+      setIsReadyForInstall(true);
+    });
+  }, []);
+
+  async function downloadApp() {
+    console.log("👍", "butInstall-clicked");
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      // The deferred prompt isn't available.
+      console.log("oops, no prompt event guardado en window");
+      return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log("👍", "userChoice", result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    setIsReadyForInstall(false);
+  }
+
+  function getOperatingSystem() {
+    if (window.navigator.appVersion.indexOf('Win') !== -1) { setIsIOS(false) }
+    if (window.navigator.appVersion.indexOf('Mac') !== -1) { setIsIOS(true) }
+    if (window.navigator.appVersion.indexOf('X11') !== -1) { setIsIOS(false) }
+    if (window.navigator.appVersion.indexOf('Linux') !== -1) { setIsIOS(false) }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        { isReadyForInstall && isIOS && <button onClick={downloadApp}>download</button> }
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,5 +66,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
