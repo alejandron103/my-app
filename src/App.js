1bfbe9d81223bcb64f7603ec7e59eec7c820ae71
alejandron103 <alejandro.naranjo@cun.edu.co>
2022-10-24 14:29:50 -0500

    remove isIos validation

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
       // Prevent the mini-infobar from appearing on mobile.
       event.preventDefault();
       console.log("👍", "beforeinstallprompt", event);
+      alert("👍", "beforeinstallprompt", event)
       // Stash the event so it can be triggered later.
       window.deferredPrompt = event;
       // Remove the 'hidden' class from the install button container.
